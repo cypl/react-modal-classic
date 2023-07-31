a054f976b1744f6a37c732d1a024737588118f8e
cypl <cyrille.plenchette@me.com>
2023-07-31 16:14:49 +0200

    add useCallBack and delay during timeOut

--- src/lib/useModal.jsx
+++ src/lib/useModal.jsx
@@ -1,22 +1,28 @@
-import { useState } from "react"
+import { useCallback, useState } from "react"
 
 export function useModal() {
   const [modalOpen, setModalOpen] = useState(false)
   const [animeOut, setAnimeOut] = useState(false)
+  const [delay, setDelay] = useState(false)
 
   const openModal = () => {
     document.getElementsByTagName('html')[0].style.overflowY = "hidden"
     setModalOpen(true)
+    if (delay) clearTimeout(delay)
+    setDelay(null)
   }
 
-  const closeModal = () => {
+  const closeModal = useCallback(() => {
+    if (delay) return
     document.getElementsByTagName('html')[0].style.overflowY = "auto"
     setAnimeOut(true)
-      setTimeout(() => {
-        setModalOpen(false)
-        setAnimeOut(false)
-    }, "600")
-  }
+    const x = setTimeout(() => {
+      setModalOpen(false)
+      setAnimeOut(false)
+      setDelay(null)
+    }, 600)
+    setDelay(x)
+  }, [])
 
   return { modalOpen, openModal, animeOut, closeModal }
 }
