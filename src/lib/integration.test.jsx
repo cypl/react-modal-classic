61d376f0826b2802a64b81cb96d7cca242e33f51
cypl <cyrille.plenchette@me.com>
2023-08-04 12:27:22 +0200

    add provider in test

--- src/lib/integration.test.jsx
+++ src/lib/integration.test.jsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import { render, screen, cleanup, fireEvent, waitFor } from '@testing-library/react'
 import { describe, expect, test, afterEach } from 'vitest'
+import { ModalProvider } from './ModalContext'
 import App from '../App'
+
+const renderWithProvider = (component) => {
+  return render(
+    <ModalProvider>{component}</ModalProvider>
+  )
+}
 
 describe('Integration test for Modal component', () => {
   
@@ -9,10 +16,10 @@ describe('Integration test for Modal component', () => {
 
   test('Modal should open when button is clicked', async () => {
     
-    render(<App />)
+    renderWithProvider(<App />)
     const openModalButton = screen.getByText('Open modal')
     fireEvent.click(openModalButton)
-    const modalContent = screen.getByText('This modal is based on a component and a hook.')
+    const modalContent = screen.getByText('This React modal is based on a component and a context provider.')
     
     // Modal should be open after a short CSS animation
     await waitFor(() => {
@@ -23,10 +30,10 @@ describe('Integration test for Modal component', () => {
 
   test('Modal should close when escape key is pressed', async () => {
     
-    render(<App />)
+    renderWithProvider(<App />)
     const openModalButton = screen.getByText('Open modal')
     fireEvent.click(openModalButton)
-    const modalContent = screen.getByText('This modal is based on a component and a hook.')
+    const modalContent = screen.getByText('This React modal is based on a component and a context provider.')
     
     // Modal should be open after a short CSS animation
     await waitFor(() => {
@@ -38,7 +45,7 @@ describe('Integration test for Modal component', () => {
 
     // Modal should be closed after a short CSS animation
     await waitFor(() => {
-      expect(screen.queryByText('This modal is based on a component and a hook.')).to.be.null
+      expect(screen.queryByText('This React modal is based on a component and a context provider.')).to.be.null
     })
 
   })
