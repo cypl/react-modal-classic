f122f6865aabab30ae8697d707645d37d3d71f85
cypl <cyrille.plenchette@me.com>
2023-08-04 12:26:12 +0200

    modify modal contents

--- src/ModalContentFirst.jsx
+++ src/ModalContentFirst.jsx
@@ -1,4 +1,4 @@
-import { React, useContext } from 'react'
+import React, { useContext } from 'react'
 import { ModalContext } from './lib/ModalContext'
 
 function ModalContentFirst(){
@@ -12,7 +12,7 @@ function ModalContentFirst(){
                 <figcaption>© Jacquet “Un soirée ordinaire”</figcaption>
             </figure>
             <div className="modal-caption">
-                <p>This modal is based on a component and a hook.<br/>
+                <p>This React modal is based on a component <br/>and a context provider.<br/>
                     <button onClick={closeModal} className="close">Close</button>
                 </p>
             </div>
