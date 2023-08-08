2937d9070578642938324205168dbaf46daa6400
cypl <cyrille.plenchette@me.com>
2023-08-08 19:38:48 +0200

    refactorisation

--- src/lib/ModalContext.jsx
+++ src/lib/ModalContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, createContext } from 'react'
+import React, { useState, useEffect, useCallback, createContext, useRef } from 'react'
 import PropTypes from 'prop-types'
 import Modal from './Modal'
 
@@ -18,29 +18,43 @@ export const ModalProvider = ({ children }) => {
     const [modalContent, setModalContent] = useState()
     const [animeOut, setAnimeOut] = useState(false)
     const [modalOptions, setModalOptions] = useState(defaultOptions)
+    const closingTimeout = useRef(null)
+
+    useEffect(() => {
+        if(modalOpen){
+            document.body.style.overflowY = "hidden"
+            document.querySelector('main').setAttribute("aria-hidden", "true")
+        } else {
+            document.body.removeAttribute("style")
+            document.querySelector('main').removeAttribute("aria-hidden")
+        }
+    }, [modalOpen])
 
     const openModal = (content, options = {}) => {
-        document.getElementsByTagName('html')[0].style.overflowY = "hidden"
-        document.getElementsByTagName('main')[0].setAttribute("aria-hidden", "true")
         // Merge default options with provided ones (if any)
         setModalOptions(prevOptions => ({
             ...prevOptions,
             ...options
         }))
         setModalOpen(true)
+        // Clear any pre-existing closing timeout to ensure modals don't overlap or interfere with each other
+        if (closingTimeout.current) clearTimeout(closingTimeout.current)
         setModalContent(content)
     }
 
     const closeModal = useCallback(() => {
-        document.getElementsByTagName('html')[0].removeAttribute("style")
-        document.getElementsByTagName('main')[0].removeAttribute("aria-hidden")
+        // If the closing timeout is still running, don't do anything
+        if (closingTimeout.current) return
         setAnimeOut(true)
-        setTimeout(() => {
+        // Schedule the modal's state reset after the animation duration (600ms)
+        closingTimeout.current = setTimeout(() => {
             setModalOpen(false)
             setModalContent()
             setModalOptions(defaultOptions)
             setAnimeOut(false)
-        }, 600)
+            closingTimeout.current = null
+        }, 600) // 600ms is the duration of the closing animation
+
     }, [])
 
     return (
