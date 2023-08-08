156c6545f3176f1a6bb965cbb1106d7a7d8c84e9
cypl <cyrille.plenchette@me.com>
2023-08-08 10:39:41 +0200

    modify content

---
+++ src/components/ModalContentSecond.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { ModalContext } from '../lib/ModalContext'
+
+function ModalContentSecond(){
+    
+    const { closeModal } = useContext(ModalContext)
+
+    return (
+        <div>
+            <figure>
+                <img src="john-mcmahon-robin.jpg" alt="image content"/>
+                <figcaption>© John Mc Mahon</figcaption>
+            </figure>
+            <div className="modal-caption">
+                <p>This is another modal, <br/>with custom styles options passed as props.<br/>
+                    <button onClick={closeModal} className="close">Close</button>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default ModalContentSecond
