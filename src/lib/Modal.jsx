7bd21e2923e6b063524d74dae6ddb45702fb7626
cypl <cyrille.plenchette@me.com>
2023-08-08 09:30:22 +0200

    fix css modal height > viewport height

--- src/lib/Modal.jsx
+++ src/lib/Modal.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState, useEffect, useRef } from "react"
+import { useEffect } from "react"
 import styled, { keyframes }from "styled-components"
 import PropTypes from 'prop-types'
 
@@ -54,6 +54,7 @@ const Background = styled.div`
     align-items:center;
     position:absolute;
     width:100%;
+    min-height:100%;
     background-color: rgba(0, 0, 0, 0.85);
     animation-name: ${fadeIn};
     animation-duration: 0.05s; 
@@ -70,7 +71,7 @@ const Background = styled.div`
 `
 const ModalContent = styled.div`
     position:relative;
-    max-width:calc(100% - 40px);
+    max-width:calc(100% - 80px);
     margin:50px 0;
     min-height:${props => props.$closebutton === "in" ? "45px" : "none"};
     width: ${props => sizeValues[props.size] || "1100px"};
@@ -125,30 +126,6 @@ const sizeValues = {
 
 function Modal({ modalOpen, animeOut, closeModal, closebutton, closebuttoncolor, size, backgroundcolor, radius, modalContent }){
     
-    const [viewportHeight, setViewportHeight] = useState("auto")
-    const [modalHeight, setModalHeight] = useState("auto")
-    const modalRef = useRef(null)
-    
-    // Scroll management based on modal height.
-    // Calculate viewport height
-    useEffect(() => {
-        setViewportHeight(window.innerHeight) // On page load
-        const handleResize = () => { setViewportHeight(window.innerHeight) } // On page resize
-        window.addEventListener("resize", handleResize)
-        return () => { window.removeEventListener("resize", handleResize) }
-      }, [modalOpen])
-
-    // Calculate Modal height
-    const calculateModalHeight = () => {
-        modalRef.current && setModalHeight(modalRef.current.scrollHeight + 100)
-    }
-    useEffect(() => {
-        calculateModalHeight() // On page load
-        const handleResize = () => { calculateModalHeight() } // On page resize
-        window.addEventListener("resize", handleResize)
-        return () => { window.removeEventListener("resize", handleResize) }
-    }, [modalOpen])
-
     // close Modal on keyboard escape
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -158,29 +135,28 @@ function Modal({ modalOpen, animeOut, closeModal, closebutton, closebuttoncolor,
         return () => { document.removeEventListener('keydown', handleKeyDown) }
     }, [])
 
-    return(<>
-        {modalOpen && modalContent &&
-            <Overlay role="dialog" aria-modal="true" aria-hidden="false">
-                <Background 
-                    onClick={() => closeModal()}
-                    style={{ height: `${(viewportHeight < modalHeight) ? `${modalHeight}px` : '100%'}` }}
-                    className={animeOut ? 'anime-out' : ''}
-                >
-                    <ModalContent ref={modalRef} $closebutton={closebutton} size={size} className={animeOut ? 'anime-out' : ''}>
-                        <ModalContentBackground style={{backgroundColor : `${(backgroundcolor != undefined) ? `${backgroundcolor}` : "#fff"}`, borderRadius:`${(radius != undefined) ? `${radius}` : `none`}`}}>
-                            {closebutton != "none" && 
-                                <CloseModal onClick={() => closeModal()} $closebutton={closebutton}>
-                                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" style={{color:`${(closebuttoncolor != undefined) ? `${closebuttoncolor}` : `rgba(255,255,255,0.7)`}`}} xmlns="http://www.w3.org/2000/svg"><path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
-                                </CloseModal>
-                            }
-                        </ModalContentBackground>
-                        <ModalContentInner style={{borderRadius:`${(radius != undefined) ? `${radius}` : `none`}`}}>
-                            {modalContent}
-                        </ModalContentInner>
-                    </ModalContent>
-                </Background>
-            </Overlay>
-        }
+    return(
+        <>
+            {modalOpen && modalContent &&
+                <Overlay role="dialog" aria-modal="true" aria-hidden="false">
+                    <Background onClick={() => closeModal()} className={animeOut ? 'anime-out' : ''}>
+                        <ModalContent $closebutton={closebutton} size={size} className={animeOut ? 'anime-out' : ''}>
+                            <ModalContentBackground style={{backgroundColor : `${(backgroundcolor != undefined) ? `${backgroundcolor}` : "#fff"}`, borderRadius:`${(radius != undefined) ? `${radius}` : `none`}`}}>
+                                {closebutton != "none" && 
+                                    <CloseModal onClick={() => closeModal()} $closebutton={closebutton}>
+                                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" style={{color:`${(closebuttoncolor != undefined) ? `${closebuttoncolor}` : `rgba(255,255,255,0.7)`}`}} xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
+                                        </svg>
+                                    </CloseModal>
+                                }
+                            </ModalContentBackground>
+                            <ModalContentInner style={{borderRadius:`${(radius != undefined) ? `${radius}` : `none`}`}}>
+                                {modalContent}
+                            </ModalContentInner>
+                        </ModalContent>
+                    </Background>
+                </Overlay>
+            }
         </>
     )
 }
